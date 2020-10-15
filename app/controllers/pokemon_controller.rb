df2e39770d6022508bcafce2686508c0f81772ec
Sean <Sransonette@gmail.com>
2020-10-14 22:10:10 -0500

    Login and Create user was generated

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,37 +1,58 @@
 class PokemonController < ApplicationController
 
-  # GET: /pokemon
+  
   get "/pokemon" do
-    erb :"/pokemon/index.html"
+    if logged_in?
+      @trainer = current_user
+      @pokemon = Pokemon.all 
+    else
+      redirect to "/login"
+    end
+    erb :"/pokemon/index"
   end
 
-  # GET: /pokemon/new
   get "/pokemon/new" do
-    erb :"/pokemon/new.html"
+    if logged_in?
+      erb :"/pokemon/new"
+    else
+      redirect to '/login'
+    end
   end
 
-  # POST: /pokemon
-  post "/pokemon" do
-    redirect "/pokemon"
+  post '/pokemon' do 
+    if !params[:name].blank?
+      @pokemon = Pokemon.new(name: params[:name])
+      @pokemon.trainer = current_user
+      @pokemon.save
+      redirect to "/pokemon/#{@pokemon.id}"
+    else
+      redirect to "/pokemon/new"
+    end
   end
 
-  # GET: /pokemon/5
-  get "/pokemon/:id" do
-    erb :"/pokemon/show.html"
-  end
 
-  # GET: /pokemon/5/edit
-  get "/pokemon/:id/edit" do
-    erb :"/pokemon/edit.html"
-  end
+  # # POST: /pokemon
+  # post "/pokemon" do
+  #   redirect "/pokemon"
+  # end
 
-  # PATCH: /pokemon/5
-  patch "/pokemon/:id" do
-    redirect "/pokemon/:id"
-  end
+  # # GET: /pokemon/5
+  # get "/pokemon/:id" do
+  #   erb :"/pokemon/show"
+  # end
 
-  # DELETE: /pokemon/5/delete
-  delete "/pokemon/:id/delete" do
-    redirect "/pokemon"
-  end
+  # # GET: /pokemon/5/edit
+  # get "/pokemon/:id/edit" do
+  #   erb :"/pokemon/edit"
+  # end
+
+  # # PATCH: /pokemon/5
+  # patch "/pokemon/:id" do
+  #   redirect "/pokemon/:id"
+  # end
+
+  # # DELETE: /pokemon/5/delete
+  # delete "/pokemon/:id/delete" do
+  #   redirect "/pokemon"
+  # end
 end
--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,37 +1,84 @@
 class TrainersController < ApplicationController
 
-  # GET: /trainers
-  get "/trainers" do
-    erb :"/trainers/index.html"
+  get '/signup' do
+    if logged_in?
+      redirect to "/show"
+    else 
+      erb :"/trainers/create_t"
+    end
   end
 
-  # GET: /trainers/new
-  get "/trainers/new" do
-    erb :"/trainers/new.html"
+  post '/signup' do 
+    @trainer = Trainer.new(params)
+    if @trainer.username == "" || @trainer.email == "" || @trainer.password == ""
+      redirect to "/signup"
+    elsif @trainer.save
+      session[:id] = @trainer.id
+      redirect to "/show"
+    else
+      redirect to "/signup"
+    end
   end
 
-  # POST: /trainers
-  post "/trainers" do
-    redirect "/trainers"
+  get '/login' do 
+    if logged_in?
+      current_user
+      redirect to "/show"
+    else
+      erb :'/trainers/login'
+    end
   end
 
-  # GET: /trainers/5
-  get "/trainers/:id" do
-    erb :"/trainers/show.html"
+  post '/login' do 
+    @trainer = Trainer.find_by(:username => params[:username])
+
+    if @trainer && @trainer.authenticate(params[:password])
+      session[:id] = @trainer.id 
+      redirect to "/show"
+    else
+      redirect to "/login"
+    end
   end
 
-  # GET: /trainers/5/edit
-  get "/trainers/:id/edit" do
-    erb :"/trainers/edit.html"
+  get '/logout' do 
+    if logged_in?
+      session.clear
+    end
+    redirect to "/login"
   end
 
-  # PATCH: /trainers/5
-  patch "/trainers/:id" do
-    redirect "/trainers/:id"
+  get '/show' do 
+    @trainer = current_user
+    erb :'/trainers/show'
   end
 
-  # DELETE: /trainers/5/delete
-  delete "/trainers/:id/delete" do
-    redirect "/trainers"
-  end
+#   # GET: /trainers/create_t
+#   get "/trainers/create_t" do
+#     erb :"/trainers/create_t"
+#   end
+
+#   # POST: /trainers
+#   post "/trainers" do
+#     redirect "/trainers"
+#   end
+
+#   # GET: /trainers/5
+#   get "/trainers/:id" do
+#     erb :"/trainers/show"
+#   end
+
+#   # GET: /trainers/5/edit
+#   get "/trainers/:id/edit" do
+#     erb :"/trainers/edit"
+#   end
+
+#   # PATCH: /trainers/5
+#   patch "/trainers/:id" do
+#     redirect "/trainers/:id"
+#   end
+
+#   # DELETE: /trainers/5/delete
+#   delete "/trainers/:id/delete" do
+#     redirect "/trainers"
+#   end
 end
